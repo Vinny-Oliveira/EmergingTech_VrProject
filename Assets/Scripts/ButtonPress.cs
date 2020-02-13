d3007f00ddcaf11beafc1e20eaca2893e914e159
Vinicius Oliveira <vr.sena@gmail.com>
2020-02-13 18:35:38 -0500

    Tests with button pressing

---
+++ Assets/Scripts/ButtonPress.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPress : MonoBehaviour
+{
+    public Text Txt_TestMsg;
+
+    public void DisplayMessage() {
+        Txt_TestMsg.text = "CLICKED!!!";
+    }
+}
