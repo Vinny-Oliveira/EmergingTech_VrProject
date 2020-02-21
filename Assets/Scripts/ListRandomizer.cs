f9ef614dfa0eaccc5a5ce0bd2cd91f7d5c9d8723
Vinicius Oliveira <vr.sena@gmail.com>
2020-02-20 20:10:57 -0500

    Method Randomizer test

---
+++ Assets/Scripts/ListRandomizer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+/* Randomize the order of items in a list */
+public class ListRandomizer {
+
+    public static void Randomize<T>(List<T> listItems) {
+        System.Random rand = new System.Random();
+
+        // For each spot in the list, pick a random item to swap into that spot.
+        for (int i = 0; i < listItems.Count - 1; i++) {
+            int j = rand.Next(i, listItems.Count);
+            T temp = listItems[i];
+            listItems[i] = listItems[j];
+            listItems[j] = temp;
+        }
+    }
+}
---
+++ Assets/Scripts/MethodManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethodManager : MonoBehaviour
+{
+    delegate void MyMethod();
+    public List<VRButton> listButtons = new List<VRButton>();
+    
+    // Start is called before the first frame update
+    void Start() {
+        List<MyMethod> listMethods = new List<MyMethod>();
+
+        listMethods.Add(DoFirstThing);
+        listMethods.Add(DoSecondThing);
+        listMethods.Add(DoThirdThing);
+
+        ListRandomizer.Randomize(listMethods);
+
+        for (int i = 0; i < listButtons.Count; i++) {
+            VRButton button = listButtons[i];
+            button.ButtonListeners.AddListener(DoFirstThing);
+        }
+    }
+
+    private void DoFirstThing(VRButton arg0)
+    {
+        throw new NotImplementedException();
+    }
+
+    void DoFirstThing() {
+        Debug.Log("1st thing done");
+    }
+
+    void DoSecondThing() {
+        Debug.Log("2nd thing done");
+    }
+
+    void DoThirdThing() {
+        Debug.Log("3rd thing done");
+    }
+}
