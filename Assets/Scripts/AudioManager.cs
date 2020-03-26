d1e7defd2b738f36c618b3c4300cd4bac6132ab8
Vinicius Oliveira <vr.sena@gmail.com>
2020-03-26 12:59:09 -0400

    Changed the main scene name from "ViniScene" to "OficialScene"

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class AudioManager : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public AudioSource audioButton;
 }
