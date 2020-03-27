8abeb91d3e2a10cbea2555288fbaa77c347b4d03
Steven Mirasola <scmirasola@gmail.com>
2020-03-26 22:11:28 -0400

    Changes from Oz Meeting

---
+++ Assets/Scripts/DebugThrow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugThrow : MonoBehaviour
+{
+    public float force = 10;
+    public void Throw()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
+    }
+}
