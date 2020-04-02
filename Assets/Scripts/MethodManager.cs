eaf349b47f56250fc8b27dcfd5bf45a617a452e7
Vinicius Oliveira <vr.sena@gmail.com>
2020-04-02 16:34:37 -0400

    Buttons can open the panel and fix the pipe

--- Assets/Scripts/MethodManager.cs
+++ Assets/Scripts/MethodManager.cs
@@ -5,28 +5,12 @@ using UnityEngine;
 using UnityEngine.Events;
 using System.Linq;
 using Valve.VR.InteractionSystem;
+//using MethodUtil;
 
 /// <summary>
 /// Manage how random methids are distributed to the buttons of the game
 /// </summary>
-public class MethodManager : MonoBehaviour
-{
-    //public List<VRButton> listButtons = new List<VRButton>();
-    public GameObject buttonHolder;
-    public GameObject sphereHolder;
-    //public GameObject fireHolder;
-
-    //public GameObject ExplodingObj;
-
-    public Transform player;
-
-    // Winning condition sequence
-    bool isPanel_Open = false;
-    bool isPressureReleased = false;
-    bool isDoorOpen = false;
-
-    //[SerializeField]
-    //List<Transform> listFire = new List<Transform>();
+public class MethodManager : MethodUtil {
 
     // Start is called before the first frame update
     void Start() {
@@ -37,10 +21,6 @@ public class MethodManager : MonoBehaviour
 
         // Reset button list
         List<HoverButton> listButtons = buttonHolder.GetComponentsInChildren<HoverButton>().ToList();
-
-        ////listFire = fireHolder.GetComponentsInChildren<Transform>().ToList();
-        //fireHolder.GetComponentsInChildren<Transform>(listFire);
-        //listFire.RemoveAt(0); // Remove parent
 
         // List of methods to be called by the in-game panels
         List<UnityAction<Hand>> listActions = new List<UnityAction<Hand>> {
@@ -74,83 +54,4 @@ public class MethodManager : MonoBehaviour
             button.onButtonIsPressed.AddListener(unityActions[index]);
         }
     }
-
-    #region EVENT_FUNCTIONS
-
-    /// <summary>
-    /// Have spheres drop from the ceiling
-    /// </summary>
-    /// <param name="button"></param>
-    void DropSpheres(Hand button) {
-        sphereHolder.SetActive(true);
-        Debug.Log("Balls");
-    }
-
-    ///// <summary>
-    ///// Rotate the player away from the panel
-    ///// </summary>
-    ///// <param name="button"></param>
-    //void RotatePlayer(VRButton button) {
-    //    float randomNum = UnityEngine.Random.Range(160f, 200f);
-    //    player.localRotation *= Quaternion.Euler(0f, randomNum, 0f);
-    //    Debug.Log("Rotate");
-    //}
-
-    public void ExplodeObject(Hand button) {
-        Debug.Log("Explosion");
-    }
-
-    public void SetFireOff(Hand button) {
-        //listFire.ElementAt(0).gameObject.SetActive(false);
-        //listFire.RemoveAt(0);
-        Debug.Log("Fire Off");
-    }
-
-    void PlayDisasterAnimation(Hand button) {
-        // TODO: Play disaster animation on the security panels
-        Debug.Log("Disaster played");
-    }
-
-    #endregion
-
-    #region BEEPS_AND_BOOPS_FUNCTIONS
-
-    void PlayButtonSound(Hand button) {
-        SfxManager.instance.PlayButtonSound();
-        Debug.Log("BEEP & BOOP");
-    }
-
-    #endregion
-
-    #region WINNING_CONDITION_FUNCTIONS
-
-    void OpenPanel(Hand button) {
-        // TODO: Open the panel
-        isPanel_Open = true;
-        Debug.Log("Panel door is open");
-    }
-
-    void ReleasePressure(Hand button) {
-        if (!isPanel_Open) {
-            PlayButtonSound(button);
-            return;
-        }
-
-        // TODO: Release the preasure of the pipe
-        isPressureReleased = true;
-        Debug.Log("Pipe preassure has been released");
-    }
-
-    void OpenDoors(Hand button) { 
-        if (!isPanel_Open || !isPressureReleased) {
-            PlayButtonSound(button);
-            return;
-        }
-
-        // TODO: Open the doors and win the game
-        isDoorOpen = true;
-        Debug.Log("Exit doors are open");
-    }
-
-    #endregion
 }
